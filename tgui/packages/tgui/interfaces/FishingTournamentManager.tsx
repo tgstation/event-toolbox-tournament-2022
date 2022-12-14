27dd8bdfd487a9ac7d874b09c11d395bfaa0067e
RigglePrime <27156122+RigglePrime@users.noreply.github.com>
2022-12-14 23:50:19 +0100

    Cleanup

--- tgui/packages/tgui/interfaces/FishingTournamentManager.tsx
+++ tgui/packages/tgui/interfaces/FishingTournamentManager.tsx
@@ -22,54 +22,58 @@ export const FishingTournamentManager = (props, context) => {
               {tournament || 'Create new'}
             </Button>
             {!!tournament && (
+              <>
               <Button onClick={() => act('open_VV')}>
                 VV
               </Button>
-            )}
-            {!!tournament && !!tournament_going_on && (
-              <LabeledList.Item label="Time left">
-                <TimeDisplay value={time_left} auto="down" />
-              </LabeledList.Item>
+              {!!tournament_going_on && (
+                <LabeledList.Item label="Time left">
+                  <TimeDisplay value={time_left} auto="down" />
+                </LabeledList.Item>
+              )}
+              </>
             )}
           </LabeledList.Item>
-          {!tournament_going_on && (
-            <LabeledList.Item label="Tournament Duration">
-              <NumberInput
-                width="100px"
-                value={duration}
-                unit="ds"
-                minValue={0}
-                onChange={(e, value) =>
-                  act('set_duration', {
-                    duration: value,
-                  })
-                }
-              />
-            </LabeledList.Item>
-          )}
           {!!tournament && (
-            <LabeledList.Item label="Actions">
-                <Button onClick={() => act('get')}>Get</Button>
-                {!tournament_going_on && (
-                  <Button
-                    content='Start Tournament'
-                    color='green'
-                    onClick={() => act('start')}
-                  />
-                )}
-                {!!tournament_going_on && (
-                  <Button
-                    content='Stop Tournament Now'
-                    color='yellow'
-                    onClick={() => act('stop')}
-                  />
-                )}
-                <Button
-                  content='Delete'
-                  color='red'
-                  onClick={() => act('delete')}
+            <>
+            {!tournament_going_on && (
+              <LabeledList.Item label="Tournament Duration">
+                <NumberInput
+                  width="100px"
+                  value={duration}
+                  unit="ds"
+                  minValue={0}
+                  onChange={(e, value) =>
+                    act('set_duration', {
+                      duration: value,
+                    })
+                  }
                 />
-            </LabeledList.Item>
+              </LabeledList.Item>
+              )}
+              <LabeledList.Item label="Actions">
+                  <Button onClick={() => act('get')}>Get</Button>
+                  {!tournament_going_on && (
+                    <Button
+                      content='Start Tournament'
+                      color='green'
+                      onClick={() => act('start')}
+                    />
+                  )}
+                  {!!tournament_going_on && (
+                    <Button
+                      content='Stop Tournament Now'
+                      color='yellow'
+                      onClick={() => act('stop')}
+                    />
+                  )}
+                  <Button
+                    content='Delete'
+                    color='red'
+                    onClick={() => act('delete')}
+                  />
+              </LabeledList.Item>
+            </>
           )}
         </LabeledList>
       </Window.Content>
